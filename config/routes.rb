ca721c05d40c822a987fb7e8d6b108f97df97014
jameskwapien <james.m.kwapien@wmich.edu>
2016-01-25 06:25:40 +0000

    controls progress

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
     root to: "devise/sessions#new"
   end
   
-  devise_for :users
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  resources :users
   
   get 'instructor/index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,54 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123030713) do
+ActiveRecord::Schema.define(version: 20160125050308) do
+
+  create_table "blogit_comments", force: :cascade do |t|
+    t.string   "name",       limit: 255,   null: false
+    t.string   "email",      limit: 255,   null: false
+    t.string   "website",    limit: 255
+    t.text     "body",       limit: 65535, null: false
+    t.integer  "post_id",    limit: 4,     null: false
+    t.string   "state",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "blogit_comments", ["post_id"], name: "index_blogit_comments_on_post_id", using: :btree
+
+  create_table "blogit_posts", force: :cascade do |t|
+    t.string   "title",          limit: 255,                     null: false
+    t.text     "body",           limit: 65535,                   null: false
+    t.string   "state",          limit: 255,   default: "draft", null: false
+    t.integer  "comments_count", limit: 4,     default: 0,       null: false
+    t.integer  "blogger_id",     limit: 4
+    t.string   "blogger_type",   limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "description",    limit: 65535
+  end
+
+  add_index "blogit_posts", ["blogger_type", "blogger_id"], name: "index_blogit_posts_on_blogger_type_and_blogger_id", using: :btree
+
+  create_table "taggings", force: :cascade do |t|
+    t.integer  "tag_id",        limit: 4
+    t.integer  "taggable_id",   limit: 4
+    t.string   "taggable_type", limit: 255
+    t.integer  "tagger_id",     limit: 4
+    t.string   "tagger_type",   limit: 255
+    t.string   "context",       limit: 128
+    t.datetime "created_at"
+  end
+
+  add_index "taggings", ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true, using: :btree
+  add_index "taggings", ["taggable_id", "taggable_type", "context"], name: "index_taggings_on_taggable_id_and_taggable_type_and_context", using: :btree
+
+  create_table "tags", force: :cascade do |t|
+    t.string  "name",           limit: 255
+    t.integer "taggings_count", limit: 4,   default: 0
+  end
+
+  add_index "tags", ["name"], name: "index_tags_on_name", unique: true, using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "",    null: false
@@ -27,6 +74,7 @@ ActiveRecord::Schema.define(version: 20160123030713) do
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
     t.boolean  "instructor",                         default: false
+    t.string   "name",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
