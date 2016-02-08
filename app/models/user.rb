f3d8453db4e2781028033c760df8be5849ed5e7e
jameskwapien <james.m.kwapien@wmich.edu>
2016-02-08 05:07:26 +0000

    instructor group views

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,25 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
   has_many :memberships
   has_many :groups, through: :memberships
+
+  def self.instructor
+    User.where(instructor: true)
+  end
+
+  def self.student
+    User.where(instructor: false)
+  end
+
+  def self.not_in_any_group
+    User.includes(:memberships).where(:memberships => {:user_id => nil})
+  end  
+
+  def self.in_a_group
+    User.includes(:memberships).where(:memberships => {:user_id => !nil})
+  end
+
+  def membership?(group)
+    memberships.find_by(group: group).present?
+  end
+
 end
